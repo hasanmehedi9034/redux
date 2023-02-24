f9d4d9d51d918a43b7c364135bdfe88df14202c8
hasanmehedi9034 <mehedi90344@gmail.com>
2023-02-25 01:31:07 +0600

    action and actioyType created in redux

---
+++ 3_todo_app/my-app/src/redux/filters/actions.js
@@ -0,0 +1,20 @@
+import { COLOR_CHANGED, STATUS_CHANGED } from "./actionTypes"
+
+export  const  colorChanged = (color, changedType) => {
+    return {
+        type: COLOR_CHANGED,
+        payload: {
+            color,
+            changedType
+        }
+    }
+}
+
+export  const  statusChanged = (status) => {
+    return {
+        type: STATUS_CHANGED,
+        payload: {
+            status
+        }
+    }
+}
---
+++ 3_todo_app/my-app/src/redux/todos/actionTypes.js
@@ -0,0 +1,8 @@
+export const ADDED = 'todos/added';
+export const TOGGLED = 'todos/toggled';
+export const COLOR_SELETED = 'todos/color-selected';
+export const DELETED = 'todos/deleted';
+export const ALL_COMPLETED = 'todos/all-completed';
+export const CLEAR_COMPLETED = 'todos/clear-completed';
+
+
