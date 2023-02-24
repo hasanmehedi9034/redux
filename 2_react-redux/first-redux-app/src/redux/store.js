7ac0fd15136834d957f65705133ce3735aa5ef85
hasanmehedi9034 <mehedi90344@gmail.com>
2023-02-24 11:40:54 +0600

    multiple middleware use in redux

--- 2_react-redux/first-redux-app/src/redux/store.js
+++ 2_react-redux/first-redux-app/src/redux/store.js
@@ -1,22 +1,14 @@
 import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import logger from 'redux-logger';
+import myLogger from './middlewares/myLogger';
 import rootReducer from './rootReducer';
-
-// create our first middleware
-const myLogger = (store) => (next) => (action) => {
-    console.log(`Action: ${JSON.stringify(action)}`);
-    console.log(`Before: ${JSON.stringify(store.getState())}`);
-
-    const upcommingState = [action].reduce(rootReducer, store.getState);
-
-    console.log(`upcoming state: ${JSON.stringify(upcommingState)}`);
-
-    // pass action
-    return next(action);
-}
 
 const store = createStore(
     rootReducer,
-    applyMiddleware(myLogger)
+    applyMiddleware(
+        logger,
+        myLogger
+    )
 );
 
 export default store;
