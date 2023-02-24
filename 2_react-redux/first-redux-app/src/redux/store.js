cec7e3f8bd0e82ae48352cc5101af63f19bc0278
hasanmehedi9034 <mehedi90344@gmail.com>
2023-02-24 11:52:09 +0600

    debugging in redux app with redux-devtools-extention

--- 2_react-redux/first-redux-app/src/redux/store.js
+++ 2_react-redux/first-redux-app/src/redux/store.js
@@ -1,13 +1,16 @@
 import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 import myLogger from './middlewares/myLogger';
 import rootReducer from './rootReducer';
 
 const store = createStore(
     rootReducer,
-    applyMiddleware(
-        logger,
-        myLogger
+    composeWithDevTools(
+        applyMiddleware(
+            logger,
+            myLogger,
+        )
     )
 );
 
