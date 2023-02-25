a8a28a79a55de778739cb2b44c51b5899a73428e
hasanmehedi9034 <mehedi90344@gmail.com>
2023-02-25 22:01:58 +0600

    status changed implementation

--- 4_asynchronus_action/todo_app/my-app/src/components/Todo.js
+++ 4_asynchronus_action/todo_app/my-app/src/components/Todo.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import cancelIcon from '../assets/images/cancel.png'
-import { colorSelected, deleted, toggled } from '../redux/todos/actions';
+import { colorSelected, deleted } from '../redux/todos/actions';
+import updateStatus from '../redux/todos/thunk/updateTodoStatus';
 
 export default function Todo({todo}) {
     const  {text, id, completed, color} = todo;
     const dispatch = useDispatch();
 
     const handleStatusChange = (todoId) => {
-        dispatch(toggled(todoId))
+        dispatch(updateStatus(todoId, completed))
     }
 
     const handleColorChange = (todoId, priorityColor) => {
