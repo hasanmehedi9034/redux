bbe706d845053343953d18d50c1455762e4cec2a
hasanmehedi9034 <mehedi90344@gmail.com>
2023-03-12 23:36:31 +0600

    async function

--- 5_redux_toolkit/rtk/asyncAction.js
+++ 5_redux_toolkit/rtk/asyncAction.js
@@ -64,7 +64,7 @@ const fetchPosts = () => {
     return async (dispatch) => {
         dispatch(fetchPostRequested())
         try {
-            const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+            const response = await fetch('https://jsonplaceholder.typicode.co/posts?_limit=5')
             const posts = await response.json();
             dispatch(fetchPostSucceeded(posts));
         }
