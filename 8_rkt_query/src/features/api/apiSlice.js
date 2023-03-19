ed9f4e0c62420a60517ae4be564cf637938a5796
hasanmehedi9034 <mehedi90344@gmail.com>
2023-03-19 15:15:22 +0600

    video feature done

--- 8_rkt_query/src/features/api/apiSlice.js
+++ 8_rkt_query/src/features/api/apiSlice.js
@@ -1,4 +1,4 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
     reducerPath: 'api',
@@ -6,7 +6,11 @@ export const apiSlice = createApi({
         baseUrl: 'http://localhost:9000',
     }),
     endpoints: (builder) => ({
-
-    })
+        getVideos: builder.query({
+            query: () => '/videos'
+        }),
+    }),
 });
+
+export const { useGetVideosQuery } = apiSlice
 
