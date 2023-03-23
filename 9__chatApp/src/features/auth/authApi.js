43ac30c7d6325cc0f702788022afcd22b1eb0d2f
hasanmehedi9034 <mehedi90344@gmail.com>
2023-03-24 00:11:46 +0600

    action dispatch form API file

--- 9__chatApp/src/features/auth/authApi.js
+++ 9__chatApp/src/features/auth/authApi.js
@@ -1,7 +1,46 @@
 import { apiSlice } from "../api/apiSlice";
+import { userLoggedIn } from "./authSlice";
 
 export const authApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
-        
+
+        // for new account
+        register: builder.mutation({
+            query: (data) => ({
+                url: '/register',
+                method: 'POST',
+                body: data
+            }),
+            async onQueryStarted(arg, {queryFulfilled, dispatch}) {
+                try{
+                    const result = await queryFulfilled;
+                    localStorage.setItem('auth', JSON.stringify(result.data))
+                    dispatch(userLoggedIn(result.data))
+                }
+                catch(error) {}
+            }
+        }),
+
+        // for login
+        login: builder.mutation({
+            query: (data) => ({
+                url: '/login',
+                method: 'POST',
+                body: data
+            }),
+            async onQueryStarted(arg, {queryFulfilled, dispatch}) {
+                try{
+                    const result = await queryFulfilled;
+                    localStorage.setItem('auth', JSON.stringify(result.data))
+                    dispatch(userLoggedIn(result.data))
+                }
+                catch(error) {}
+            }
+        }),
     })
 })
+
+export const {
+    useLoginMutation, 
+    useRegisterMutation
+} = authApi;
