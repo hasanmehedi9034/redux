78487f7a8ff618bc5acc497e3525040c2eb56583
hasanmehedi9034 <mehedi90344@gmail.com>
2023-03-23 23:22:49 +0600

    store

--- 9__chatApp/src/app/store.js
+++ 9__chatApp/src/app/store.js
@@ -1,8 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { apiSlice } from '../features/api/apiSlice';
+import authSlice from '../features/auth/authSlice';
+import conversationsSlice from '../features/conversations/conversationsSlice';
+import messagesSlice from '../features/messages/messagesSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authSlice,
+    conversations: conversationsSlice,
+    messages: messagesSlice
   },
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddlewares) => [...getDefaultMiddlewares(), apiSlice.middleware]
 });
