9c24fd65ee680727252622b646aaa527d67c12f3
hasanmehedi9034 <mehedi90344@gmail.com>
2023-03-24 17:57:31 +0600

    messages api implement

--- 9__chatApp/src/features/messages/messagesApi.js
+++ 9__chatApp/src/features/messages/messagesApi.js
@@ -2,6 +2,12 @@ import { apiSlice } from "../api/apiSlice";
 
 export const messagesApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
-        
+        getMessages: builder.query({
+            query: (id) => `/messages?conversationId=${id}&_sort=timestapm&_order=desc&page=1&limit=${process.env.REACT_APP_MESSAGES_PER_PAGE}`
+        })
     })
 })
+
+export const {
+    useGetMessagesQuery
+} = messagesApi
